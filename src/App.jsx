16eb9fb1c5b3613ffb26b49d9709250d5696e564
Wellington <wellington_ctt@hotmail.com>
2025-06-14 10:07:54 -0300

    modificacao em rotas e pastas do diretorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './Pages/Home/Home'
+import Home from './Pages/Home/Home.jsx'
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
      <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/Portifolio/' element={<Home/>}/>
      </Routes>
     </BrowserRouter>
   )
