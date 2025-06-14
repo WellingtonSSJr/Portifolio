27c37ac5f01294dd6190d33181fa30d080a24122
Wellington <wellington_ctt@hotmail.com>
2025-06-14 09:50:19 -0300

    commit

--- Portifolio/vite.config.js
+++ Portifolio/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'PortifolioNovo',
+  base:'/Portifolio/',
   plugins: [
     react(),
     tailwindcss()
